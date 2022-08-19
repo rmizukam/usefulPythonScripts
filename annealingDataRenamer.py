ebdd077469da9c161bc45ab62ffe549ce20311e8
Ryan Mizukami <rmizukami741@gmail.com>
2022-08-18 19:54:29 -0700

    updated suppport for lab computer

--- annealingDataRenamer.py
+++ annealingDataRenamer.py
@@ -6,9 +6,15 @@ import platform
 proj = 'CuPcAnnealing'
 initials = 'RM'
 tData = 'AFM'
-folder_on_Desktop = "220816"
+folder_on_Desktop = "220817"
+inLab = True 
 # =============================================================
-if platform.system() == 'Windows':
+if inLab :
+    folder = "C:/Users/014443024/Desktop"
+    folder = os.path.join(folder, folder_on_Desktop)
+    saveFolder = 'C:/Users/014443024/OneDrive - CSULB/RyanMizukami/RAW'
+    pNotesAnnealing = 'C:/Users/014443024/OneDrive - CSULB/RyanMizukami/RProject/dataAFMrm/csv/NotesForRM20220701GL0XXSeries.csv'
+elif platform.system() == 'Windows':
     folder = "C:/Users/Rmizu/Desktop"
     folder = os.path.join(folder, folder_on_Desktop)
     saveFolder = 'C:/Users/Rmizu/Documents/OneDrive - CSULB/RyanMizukami/RAW'
@@ -24,6 +30,8 @@ pRAW = os.listdir(folder)
 # data = pandas.read_csv(pNotesAnnealing)
 for f in pRAW:
     if f.endswith('ibw') and f.startswith('RM'):
+        sample = re.findall(r'[A-Z]{2}\d{6}[A-Z]{2}\d{2}[A-Z]*', f)[0]
+        sampleName = sample[0:2] + '20' + sample[2:]
         endPhrase = re.findall(r'P[A-Za-z]{2,3}\d{2,}', f)
         prepost = re.findall(r'[A-Za-z]+', endPhrase[0])[0]
         if prepost == 'Pos':
