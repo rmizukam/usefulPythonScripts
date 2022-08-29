f08ab6712a7ed3021ba0950bd1daeeeb6e2c6f90
Ryan Mizukami <rmizukami741@gmail.com>
2022-08-29 11:42:39 -0700

    added raw path for lab pc

--- RAWsync.py
+++ RAWsync.py
@@ -4,15 +4,20 @@ from dirsync import sync
 from functions import choice_for_sync
 
 lab2Personal = True  # set as true if local folder is behind cloud folder
+inLab = True
 
 if platform.system() == 'Darwin':
     # CLOUD -> MAC
     RAWCloudPath = '/Users/rmizu/Library/CloudStorage/OneDrive-CSULB/RyanMizukami/RAW'
     RAWLocalPath = '/Users/rmizu/LocalStorage/Repositories/RAW'
 elif platform.system() == 'Windows':
-    # CLOUD -> HOME PC
-    RAWCloudPath = 'C:/Users/Rmizu/Documents/OneDrive - CSULB/RyanMizukami/RAW'
-    RAWLocalPath = 'C:/Repositories/RAW'
+    if inLab == True:
+        RAWCloudPath = 'C:/Users/014443024/OneDrive - CSULB/RyanMizukami/RAW'
+        RAWLocalPath = 'C:/Users/014443024/Desktop/Repositories/RAW'
+    else:
+        # CLOUD -> HOME PC
+        RAWCloudPath = 'C:/Users/Rmizu/Documents/OneDrive - CSULB/RyanMizukami/RAW'
+        RAWLocalPath = 'C:/Repositories/RAW'
 
 choice = choice_for_sync()
 
