8b6dc7acf83691d7ec982165cb1a7e06f1698817
Ryan Mizukami <rmizukami741@gmail.com>
2022-09-10 21:53:10 -0700

    used program

--- annealingDataRenamer.py
+++ annealingDataRenamer.py
@@ -6,7 +6,7 @@ import platform
 proj = 'CuPcAnnealing'
 initials = 'RM'
 tData = 'AFM'
-folder_on_Desktop = "220819"
+folder_on_Desktop = "220909"
 labComputer = False
 # =============================================================
 if labComputer:
@@ -30,7 +30,7 @@ pRAW = os.listdir(folder)
 # data = pandas.read_csv(pNotesAnnealing)
 for f in pRAW:
     if f.endswith('ibw') and f.startswith('RM'):
-        sample = re.findall(r'[A-Z]{2}\d{6}[A-Z]{2}\d{2}[A-Z]*', f)[0]
+        sample = re.findall(r'[A-Z][A-Z]\d{6}[A-Z]{2}\d{2}[A-Z]*', f)[0]
         sampleName = sample[0:2] + '20' + sample[2:]
         endPhrase = re.findall(r'P[A-Za-z]{2,3}\d{2,}', f)
         prepost = re.findall(r'[A-Za-z]+', endPhrase[0])[0]
